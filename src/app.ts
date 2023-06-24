580aae58bf839042406b4220ea912ef85f6e6f2e
Emmanuel Solis <emma.ruso@outlook.com>
2023-06-24 12:15:47 -0600

    Configuramos el enrutador con trpc

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
-import express from  'express' 
+import express from 'express'
 import morgan from 'morgan'
+import * as trpc from '@trpc/server'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { router, createContext} from './trpc'
 
 const app = express()
 
+const AppRouter = router({})
+
 app.use(morgan("dev"))
+
+app.use('/trpc', trpcExpress.createExpressMiddleware({
+    router: AppRouter,
+    createContext: createContext,
+}))
+
+
 export default app 
